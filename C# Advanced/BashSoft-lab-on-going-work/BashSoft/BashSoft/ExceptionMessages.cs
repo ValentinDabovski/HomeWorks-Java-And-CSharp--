4531621141ab5808a4c9bee78e69cb24324c9436
ValentinDabovski <valentindabovski@gmail.com>
2016-05-26 17:24:23 +0300

    c# lab

---
+++ C# Advanced/BashSoft-lab-on-going-work/BashSoft/BashSoft/ExceptionMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    public class ExceptionMessages
+    {
+        public const string InvalidPath = "The folder/file you are trying to access at the current adress, does not exist!";
+    }
+}
---
+++ C# Advanced/BashSoft-lab-on-going-work/BashSoft/BashSoft/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BashSoft;
+
+
+namespace BashSoft
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IOManager.TraverseDirectory(1);
+
+
+         //    Tester.CompareContent(@"C:\Users\ValCoder\Documents\test\test2.txt", @"C:\Users\ValCoder\Documents\test\test3.txt");
+
+          //  IOManager.CreateDirectoryInCurrentFolder("");
+        }
+    }
+}
+
