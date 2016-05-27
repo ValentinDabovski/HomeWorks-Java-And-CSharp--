751816522218d45d2532c96c8fdb0b7d6e45c031
ValentinDabovski <valentindabovski@gmail.com>
2016-05-27 20:21:19 +0300

    C#-Advanced-Lab-BashSoft-Exception-Handling-Update

--- C# Advanced/BashSoft-lab-on-going-work/BashSoft/IOManager.cs
+++ C# Advanced/BashSoft-lab-on-going-work/BashSoft/IOManager.cs
@@ -9,12 +9,12 @@ namespace BashSoft
 {
     public static class IOManager
     { 
-        public static void TraverseDirectory(string path)
+        public static void TraverseDirectory(int  depth)
         {
             OutputWriter.WriteEmptyLine();
-            int initialIdentation = path.Split('\\').Length;
+            int initialIdentation = SessionData.currentPath.Split('\\').Length;
             Queue<string> subFolders = new Queue<string>();
-            subFolders.Enqueue(path);
+            subFolders.Enqueue(SessionData.currentPath);
 
 
             while (subFolders.Count != 0)
@@ -22,14 +22,93 @@ namespace BashSoft
                 string currentPath = subFolders.Dequeue();
                 int identation = currentPath.Split('\\').Length - initialIdentation;
 
-                foreach (string directoryPath in Directory.GetDirectories(currentPath))
+                try
                 {
-                    subFolders.Enqueue(directoryPath);
+                    foreach (var file in Directory.GetFiles(currentPath))
+                    {
+                        int indexOfLastSlash = file.LastIndexOf("\\");
+                        string fileName = file.Substring(indexOfLastSlash);
+                        OutputWriter.WriteMessageOnNewLine(new string('-', indexOfLastSlash) + fileName);
+                    }
+
+                    foreach (string directoryPath in Directory.GetDirectories(currentPath))
+                    {
+                        subFolders.Enqueue(directoryPath);
+                    }
+
+                    OutputWriter.WriteMessageOnNewLine(string.Format("{0}{1}", new string('-', identation), currentPath));
 
                 }
 
-                OutputWriter.WriteMessageOnNewLine(string.Format("{0}{1}", new string('-', identation), currentPath));
+                catch (UnauthorizedAccessException)
+                {
+
+                    OutputWriter.DisplayException(ExceptionMessages.UnauthorizedAccessExceptionMessage);
+                }
+                
+                if (depth - identation < 0)
+                {
+                    break;
+                }
             }
         }
+
+        public static void CreateDirectoryInCurrentFolder(string name)
+        {
+            string path = Directory.GetCurrentDirectory() + "\\" + name;
+            try
+            {
+                Directory.CreateDirectory(path);
+            }
+            catch (ArgumentException)
+            {
+                OutputWriter.DisplayException(ExceptionMessages.ForbiddenSymbolsContainedInName);
+               
+            }
+
+           
+        }
+        
+
+        public static void ChangeCurrentDirectoryRelative(string relativePath)
+        {
+            if (relativePath == "..")
+            {
+                try
+                {
+                    string currentPath = SessionData.currentPath;
+                    int indexOfLastSlash = currentPath.LastIndexOf("\\");
+                    string newPath = currentPath.Substring(0, indexOfLastSlash);
+                    SessionData.currentPath = newPath;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    OutputWriter.DisplayException(ExceptionMessages.UnableToGoHigherInPartitionHierarchy);
+                    
+                }
+            }
+            else
+            {
+                string currentPath = SessionData.currentPath;
+                currentPath += "\\" + relativePath;
+                ChangeCurrentDirectoryRelative(currentPath);
+            }
+        }
+
+
+        public static void ChangeDirectoryAbsolute(string absolutePath)
+        {
+            if (!Directory.Exists(absolutePath))
+            {
+                OutputWriter.DisplayException(ExceptionMessages.InvalidPath);
+                return;
+            }
+
+            SessionData.currentPath = absolutePath;
+        }
+
+        
+
+        
     }
 }
---
+++ C# Advanced/BashSoft-lab-on-going-work/BashSoft/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BashSoft;
+
+
+namespace BashSoft
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+              IOManager.ChangeDirectoryAbsolute(@"C:User\");
+               IOManager.TraverseDirectory(20);
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            IOManager.ChangeCurrentDirectoryRelative("..");
+            //
+            //  Tester.CompareContent(@"C:\Users\ValCoder\Documents\test\test3.txt", @"C:\Users\ValCoder\Documents\test\test3.txt");
+            //  IOManager.CreateDirectoryInCurrentFolder("*2");
+
+
+
+
+        }
+    }
+}
+
