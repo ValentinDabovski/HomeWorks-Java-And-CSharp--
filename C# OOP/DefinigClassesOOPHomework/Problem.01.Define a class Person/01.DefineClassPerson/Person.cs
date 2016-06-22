1bfeccc2a695463ff7f73f1f4b063fb567fe0338
ValentinDabovski <valentindabovski@gmail.com>
2016-06-22 20:18:03 +0300

    CSharp OOP

---
+++ C# OOP/DefinigClassesOOPHomework/Problem.01.Define a class Person/01.DefineClassPerson/Person.cs
@@ -0,0 +1,17 @@
+﻿
+public class Person
+{
+    public string firstName;
+    public int age;
+
+    public Person(string firstName,int age)
+    {
+        this.FirstName = firstName;
+        this.Age = age;
+    }
+
+    public string FirstName { get; set; }
+    public int Age { get; set; }
+}
+
+
---
+++ C# OOP/DefinigClassesOOPHomework/Problem.01.Define a class Person/01.DefineClassPerson/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.DefineClassPerson
+{
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            Type personType = typeof(Person);
+            FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine(fields.Length);
+
+            var personOne = new Person("Gosho",20);
+            var personTwo = new Person("Pesho",18);
+            var personThree = new Person("Stamat", 43);
+
+        }
+    }
+}
