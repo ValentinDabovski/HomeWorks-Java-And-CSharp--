2342d11ea9dd7d6e82388e7a7e3e820c5caf7a8e
ValCoder <valentindabovski@gmail.com>
2016-09-14 19:49:19 +0200

    C# 2D_Game-Zombie-Shooter

---
+++ C#_Game-Zombie-Shooter/Shoter/Shoter/Form1.cs
@@ -0,0 +1,53 @@
+﻿#define My_Debug
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shoter
+{
+    public partial class Shooter : Form
+    {
+        private int cursX = 0;
+        private int cursY = 0;
+
+        public Shooter()
+        {
+            InitializeComponent();
+        }
+
+        private void timerGameLoop_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics dc = e.Graphics;
+
+#if My_Debug
+            TextFormatFlags flags = TextFormatFlags.Left | TextFormatFlags.EndEllipsis;
+            Font font = new System.Drawing.Font("Stemcil", 12, FontStyle.Regular);
+            TextRenderer.DrawText(dc, "x=" + cursX.ToString() + ":" + "Y=" + cursY.ToString(), font, new Rectangle(0, 0, 120, 20), SystemColors.ControlText, flags);
+#endif
+
+            base.OnPaint(e);
+        }
+
+        private void Shooter_MouseMove(object sender, MouseEventArgs e)
+        {
+            cursX = e.X;
+            cursY = e.Y;
+
+            this.Refresh();
+        }
+    }
+}
