3099a52cfbe9ad98efa40f25c2a409c8c8c2f790
ValCoder <valentindabovski@gmail.com>
2016-10-23 16:10:27 +0200

    Homowork

---
+++ C# Databases - Entity Framework Homework/SoftUniEntities/SoftUniEntities/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniEntities
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniEntities();
+            var employees = context.Employees
+                .Where(emp => emp.Department.Name == "Research and Development")
+                .OrderBy(emp => emp.Salary)
+                .ThenByDescending(emp => emp.FirstName)
+                .Select(emp => new
+                {
+                    FirstName = emp.FirstName,
+                    LastName = emp.LastName,
+                    DepartamentName = emp.Department.Name,
+                    Salary = emp.Salary
+                });
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} from {employee.DepartamentName} - {employee.Salary:F2}");
+            }
+        }
+    }
+}
